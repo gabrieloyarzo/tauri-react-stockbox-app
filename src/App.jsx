9d5afd1dc95c05f0cfa7a49f1e4713390e50b346
gabrieloyarzo <gabrieloyarzo2015@gmail.com>
2024-06-17 12:46:34 -0400

    changed App.jsx to connect to websocket server using useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Grid } from "@mui/material";
 import { ThemeProvider } from "@mui/material/styles";
 import { lightTheme, darkTheme } from "./theme/theme";
@@ -6,13 +6,48 @@ import { useTheme } from "@mui/material/styles";
 import Login from "./components/organisms/forms/Login";
 
 const App = () => {
+  useEffect(() => {
+    // Establece la conexión al servidor WebSocket
+    const socket = new WebSocket(`${import.meta.env.VITE_API_URL}`);
+
+    // Maneja eventos del WebSocket
+    socket.onopen = () => {
+        console.log('WebSocket connected');
+        // Puedes enviar mensajes al servidor aquí si es necesario
+        socket.send('Hello Server!');
+    };
+
+    socket.onmessage = (event) => {
+        console.log('Message from server:', event.data);
+        // Maneja los mensajes recibidos del servidor
+    };
+
+    socket.onclose = () => {
+        console.log('WebSocket disconnected');
+        // Puedes manejar la lógica cuando se cierra la conexión
+    };
+
+    // Cierra la conexión al desmontar el componente
+    return () => {
+        socket.close();
+    };
+}, []); // Este efecto se ejecuta solo una vez al montar el componente
+
   const theme = useTheme();
-  const themeMode = localStorage.getItem('theme');
+  const themeMode = localStorage.getItem("theme");
 
   return (
     <ThemeProvider theme={themeMode === "dark" ? darkTheme : lightTheme}>
-      <Grid container spacing={2} sx={{ minHeight: "100vh", minWidth: "720px", bgcolor: theme.palette.background.default }}>
-        <Login/>
+      <Grid
+        container
+        spacing={2}
+        sx={{
+          minHeight: "100vh",
+          minWidth: "720px",
+          bgcolor: theme.palette.background.default,
+        }}
+      >
+        <Login />
       </Grid>
     </ThemeProvider>
   );
