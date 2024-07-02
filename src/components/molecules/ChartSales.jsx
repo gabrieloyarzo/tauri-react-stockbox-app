26a34b192a88069028187eb6450b042d5878a922
NikoVilla <nikovillanueva7@gmail.com>
2024-07-02 00:10:36 -0400

    dashboard test

--- src/components/molecules/ChartSales.jsx
+++ src/components/molecules/ChartSales.jsx
@@ -1,24 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 import { Typography, Grid } from '@mui/material';
 import AnalyticApi from '../../services/api/analytic.service';
 
-const data = [
-  { month: 'Ene', amount: 100000 },
-  { month: 'Feb', amount: 120000 },
-  { month: 'Mar', amount: 90000 },
-  { month: 'Abr', amount: 110000 },
-  { month: 'May', amount: 95000 },
-  { month: 'Jun', amount: 150000 },
-  { month: 'Jul', amount: 140000 },
-  { month: 'Ago', amount: 190000 },
-  { month: 'Sep', amount: 135000 },
-  { month: 'Oct', amount: 14500 },
-  { month: 'Nov', amount: 13000 },
-  { month: 'Dic', amount: 15500 },
-];
-
 const Ventas = () => {
+  const [chartData, setChartData] = useState([]);
+  const [totalSales, setTotalSales] = useState(0); 
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await AnalyticApi.getAnalyticData();
+        const salesData = response.data.datePriceSales.map(item => ({
+          month: new Date(item.date).toLocaleString('default', { month: 'short' }),
+          amount: item.total_price,
+        }));
+
+        const monthlySales = salesData.reduce((acc, data) => {
+          const { month, amount } = data;
+          if (!acc[month]) {
+            acc[month] = 0;
+          }
+          acc[month] += amount;
+          return acc;
+        }, {});
+  
+        const allMonths = Array.from({ length: 12 }, (_, index) => {
+          const month = new Date(new Date().getFullYear(), index, 1);
+          return {
+            month: month.toLocaleString('default', { month: 'short' }),
+            amount: monthlySales[month.toLocaleString('default', { month: 'short' })] || 0,
+          };
+        });
+  
+        const totalSales = allMonths.reduce((total, data) => total + data.amount, 0);
+  
+        setChartData(allMonths);
+        setTotalSales(totalSales);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+  
+    fetchData();
+  }, []);
+  
 
   const CustomTooltip = ({ active, payload, label }) => {
     if (active && payload && payload.length) {
@@ -37,8 +63,8 @@ const Ventas = () => {
     <div style={{ width: '100%', height: 'auto'}}>
       <Typography gutterBottom align="left" sx={{fontSize: "24px", fontWeight: "bold"}}>
         Balance de ventas -
-        <Typography component="span" color="textSecondary"sx={{fontSize: "30px", fontWeight: "bold", marginLeft: "5px"}}>
-          $150.000
+        <Typography component="span" color="textSecondary" sx={{fontSize: "30px", fontWeight: "bold", marginLeft: "5px"}}>
+          {`$${totalSales}`} 
         </Typography>
       </Typography>
 
@@ -46,12 +72,12 @@ const Ventas = () => {
         <Grid item xs={12}>
           <ResponsiveContainer width="100%" height={300}>
             <LineChart
-              data={data}
+              data={chartData}
               margin={{ top: 5, right: 10, left: 7, bottom: 5 }}
             >
               <CartesianGrid strokeDasharray="3 3" />
               <XAxis dataKey="month" />
-              <YAxis/> 
+              <YAxis /> 
               {/* label={{ value: 'Ventas', angle: -90, position: 'insideLeft' }} */}
               <Tooltip content={<CustomTooltip />} />
               <Line type="monotone" dataKey="amount" stroke="#266763" activeDot={{ r: 10 }} strokeWidth={2} />
