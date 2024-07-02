827de13af272f667669e737db9580cb5adbe1612
gabrieloyarzo <gabrieloyarzo2015@gmail.com>
2024-07-02 09:21:22 -0400

    fixed saledetails modal

--- src/components/organisms/modals/SaleDetails.jsx
+++ src/components/organisms/modals/SaleDetails.jsx
@@ -1,20 +1,7 @@
-import React from "react";
 import { useTheme } from "@mui/material/styles";
 import { Box, Typography, Button } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
-
-const saleDetails = {
-  folio: "N-123",
-  productId: "12347321",
-  totalSale: "100,000",
-  customerName: "Carlos Ramirez",
-  productName: "Lápiz",
-  quantity: 10,
-  unitPrice: 10000,
-  sellerName: "Juan Lama",
-  date: "10/10/10",
-  time: "13:43",
-};
+import { formatNumber } from "../../../functions/helpers";
 
 const SaleDetails = ({ data, closeModal }) => {
   const theme = useTheme();
@@ -22,10 +9,11 @@ const SaleDetails = ({ data, closeModal }) => {
   return (
     <Box
       sx={{
+        zIndex: 1,
         position: "absolute",
+        top: "50%",
         left: "50%",
-        transform: "translateX(-25%)",
-        top: "20%",
+        transform: "translate(-25%, -50%)",
         width: "80vw",
         maxWidth: "600px",
         maxHeight: "90vh",
@@ -47,41 +35,64 @@ const SaleDetails = ({ data, closeModal }) => {
         }}
       >
         <Box>
-          <Typography variant="h5" sx={{ mb: 1, pl: 3, pt: 3, pb: 1.5 }}>
+          <Typography variant="h5" sx={{ mb: 0, pl: 3, pt: 4, pb: 1.5 }}>
             Total venta:
           </Typography>
-          <Typography variant="h4" sx={{ mb: 1, pl: 3, pb: 3 }}>
-            ${saleDetails.totalSale}
+          <Typography variant="h4" sx={{ mb: 0, pl: 3, pb: 3 }}>
+            {`$ ${formatNumber(data.total)}`}
           </Typography>
         </Box>
         <Box>
-          <Typography sx={{ textAlign: "right", pr: 3 }}>
-            Nº Folio: {saleDetails.folio}
+          <Typography sx={{ textAlign: "right", pr: 3}}>
+            Código: {data.cod}
           </Typography>
           <Typography sx={{ textAlign: "right", pr: 3 }}>
-            ID de producto: {saleDetails.productId}
+            Fecha: {data.fecha}
           </Typography>
         </Box>
       </Box>
 
       <Box sx={{ p: 5 }}>
         <Typography>
-          <strong>Nombre cliente:</strong> {saleDetails.customerName}
+          <strong>RUT del cliente:</strong> {data.rutc}
         </Typography>
         <Typography>
-          <strong>Nombre producto:</strong> {saleDetails.productName}
+          <strong>RUT del usuario:</strong> {data.rutu}
         </Typography>
-        <Typography>
-          <strong>Cantidad:</strong> {saleDetails.quantity}
+
+        <hr style={{ borderTop: "1px solid grey", marginBottom: "10px", marginTop: "15px" }} /> {/* LINEA */}
+
+        <Box sx={{ py: 1, fontWeight: "bold", textAlign: "center"}}>
+        <Typography component="div">
+          <Box>
+            <span style={{ width: "20%", display: "inline-block", fontWeight: "bold", textAlign: "center" }}>Código del producto</span>
+            <span style={{ width: "27%", display: "inline-block", fontWeight: "bold", textAlign: "center" }}>Cantidad</span>
+            <span style={{ width: "25%", display: "inline-block", fontWeight: "bold", textAlign: "center" }}>Precio c/u</span>
+            <span style={{ width: "25%", display: "inline-block", fontWeight: "bold", textAlign: "center" }}>Subtotal</span>
+          </Box>
         </Typography>
-        <Typography>
-          <strong>Precio unitario:</strong> ${saleDetails.unitPrice}
-        </Typography>
-        <Typography>
-          <strong>Vendedor:</strong> {saleDetails.sellerName}
-        </Typography>
-        <Typography>
-          <strong>Fecha y hora:</strong> {saleDetails.date} - {saleDetails.time}
+      </Box>
+      
+      <Box sx={{ maxHeight: "150px", overflowY: "auto" }}>
+        <table style={{ width: "100%" }}>
+          <tbody>
+            {data.detalles.map((item, index) => (
+              <tr key={index}>
+                <td style={{ textAlign: "center", width: "20%" }}>{item.cod}</td>
+                <td style={{ textAlign: "center", width: "20%" }}>{formatNumber(item.cit)}</td>
+                <td style={{ textAlign: "center", width: "1%"}}>x</td>
+                <td style={{ textAlign: "center", width: "25%" }}> {`$ ${formatNumber(item.precio)}`}</td>
+                <td style={{ textAlign: "center", width: "20%" }}> {`$ ${formatNumber(item.suma)}`}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </Box>
+
+        <hr style={{ borderTop: "1px solid grey", marginBottom: "15px",  marginTop: "20px" }} /> {/* LINEA */}
+
+        <Typography variant="h6" sx={{ mt: 1, textAlign: "Right", marginRight: "25px", fontSize: "17px" }}>
+          {`Total: $ ${formatNumber(data.total)}`}
         </Typography>
       </Box>
 
