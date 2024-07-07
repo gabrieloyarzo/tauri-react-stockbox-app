902fa83649f2468d8a5a2a07908c213aaddad377
NikoVilla <nikovillanueva7@gmail.com>
2024-07-07 16:10:47 -0400

    cards ready

--- src/components/molecules/Cards.jsx
+++ src/components/molecules/Cards.jsx
@@ -1,8 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { Grid, Typography, Box } from '@mui/material';
 import AnalyticApi from '../../services/api/analytic.service';
+import { formatNumber } from '../../functions/helpers';
 
 const CardFormat = ({ titulo, monto, incremento, periodo }) => {
+  let incrementoColor = 'green';
+
+  if (incremento && incremento.includes('↓')) {
+    incrementoColor = 'red';
+    }
+
   return (
     <Box
       sx={{ 
@@ -31,7 +38,7 @@ const CardFormat = ({ titulo, monto, incremento, periodo }) => {
         {(incremento || periodo) && (
           <Grid item xs={12} sm={6} sx={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-end' }}>
             {incremento && (
-              <Typography fontSize="18px" color="text.secondary">
+              <Typography fontSize="18px" color={incrementoColor}>
                 {incremento}
               </Typography>
             )}
@@ -57,36 +64,72 @@ const CardGrid = () => {
     NProductos: 0
   });
 
+  const [currentMonthSales, setCurrentMonthSales] = useState(0);
+  const [lastMonthSales, setLastMonthSales] = useState(0);
+
   useEffect(() => {
-    const fetchAnalyticsData = async () => {
+    const fetchData = async () => {
       try {
-        const AnalyticData = await AnalyticApi.getAnalyticData();
-        
+        const response = await AnalyticApi.getAnalyticData();
+        const salesData = response.data.datePriceSales.map(item => ({
+          month: new Date(item.date).toLocaleString('default', { month: 'short' }),
+          amount: item.total_price,
+        }));
+
+        const monthlySales = salesData.reduce((acc, data) => {
+          const { month, amount } = data;
+          if (!acc[month]) {
+            acc[month] = 0;
+          }
+          acc[month] += amount;
+          return acc;
+        }, {});
+
+        const currentMonth = new Date().toLocaleString('default', { month: 'short' });
+        const lastMonth = new Date(new Date().getFullYear(), new Date().getMonth() - 1, 1).toLocaleString('default', { month: 'short' });
+
+        const currentSales = monthlySales[currentMonth] || 0;
+        const lastSales = monthlySales[lastMonth] || 0;
+
+        setCurrentMonthSales(currentSales);
+        setLastMonthSales(lastSales);
+
         setAnalyticsData({
-          IngresosTotales: AnalyticData.data.sumTotalSales,
-          NVentas: AnalyticData.data.countSales,
-          NCompras: AnalyticData.data.countPurchases,
-          NProveedores: AnalyticData.data.countProviders,
-          NProductos: AnalyticData.data.catProducts,
+          IngresosTotales: response.data.sumTotalSales,
+          NVentas: response.data.countSales,
+          NCompras: response.data.countPurchases,
+          NProveedores: response.data.countProviders,
+          NProductos: response.data.catProducts,
         });
       } catch (error) {
-        console.error("Error al obtener datos de analytics:", error);
+        console.error('Error fetching data:', error);
       }
     };
 
-    fetchAnalyticsData();
+    fetchData();
   }, []);
+
+  let incrementoIngresos = '';
+  if (currentMonthSales !== 0 && lastMonthSales !== 0) {
+    const incremento = ((currentMonthSales - lastMonthSales) / lastMonthSales) * 100;
+    incrementoIngresos = `${incremento.toFixed(0)}% `;
+    if (incremento < 0) {
+      incrementoIngresos += '↓';
+    } else {
+      incrementoIngresos += '↑';
+    }
+  }
 
   return (
     <Grid container spacing={2}>
       <Grid item xs={2.4}>
-        <CardFormat titulo="Ingresos totales" monto={`$${analyticsData.IngresosTotales}`} incremento="10% ↑" periodo="Último mes" />
+        <CardFormat titulo="Ingresos totales" monto={`$${formatNumber(currentMonthSales)}`} incremento={incrementoIngresos} periodo="Último mes" />
       </Grid>
       <Grid item xs={2.4}>
-        <CardFormat titulo="N° de ventas" monto={analyticsData.NVentas} incremento="10% ↑" periodo="Último mes" />
+        <CardFormat titulo="N° de ventas" monto={analyticsData.NVentas} />
       </Grid>
       <Grid item xs={2.4}>
-        <CardFormat titulo="N° de compras" monto={analyticsData.NCompras} incremento="10% ↑" periodo="Último mes" />
+        <CardFormat titulo="N° de compras" monto={analyticsData.NCompras} />
       </Grid>
       <Grid item xs={2.4}>
         <CardFormat titulo="N° de proveedores" monto={analyticsData.NProveedores} />
